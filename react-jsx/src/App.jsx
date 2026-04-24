ae7009332a025da0bebff5ddd44fd09b24e7d26f
Satyanarayan Rout <101426863+kunal101994@users.noreply.github.com>
2026-04-24 19:03:54 +0530

    css added

--- react-jsx/src/App.jsx
+++ react-jsx/src/App.jsx
@@ -1,14 +1,11 @@
-import "./App.js";
-
+import "./App.css";
 
 const App = () => {
-  return(
-    
+  return (
     <div>
       <h1>Welcome to React JSX</h1>
     </div>
-  
-  )
-}
+  );
+};
 
 export default App;
