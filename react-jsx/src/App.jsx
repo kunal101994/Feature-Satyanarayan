09ede71ef11e0af45dc2f3c2a433a2df22be7f06
Satyanarayan Rout <101426863+kunal101994@users.noreply.github.com>
2026-04-24 19:02:21 +0530

    added image

--- react-jsx/src/App.jsx
+++ react-jsx/src/App.jsx
@@ -1,11 +1,13 @@
-import "./App.js";
+import "./App.css";
 
 
 const App = () => {
   return(
-    
     <div>
       <h1>Welcome to React JSX</h1>
+      <div>
+        <img src="https://images.pexels.com/photos/754274/pexels-photo-754274.jpeg" alt="rose" height="40%" width="40%" />
+      </div>
     </div>
   
   )
